6ad86381cd2bb0c97eb6e0d47c9fd56e27d92385
Bartłomiej Kuk <98821199+PEXEL2002@users.noreply.github.com>
2025-05-27 19:15:04 +0200

    Delete Lab5 directory

--- Lab5/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Problem test = new Problem(123, 1, 10, 5);
-        System.out.println(test);
-        System.out.println(test.Solve(10));
-    }
-}
